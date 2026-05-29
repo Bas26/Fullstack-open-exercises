bb37be1f27485a03d64d97659959607b9319ff9b
Bas26 <81079172+Bas26@users.noreply.github.com>
2026-05-29 15:29:00 +0700

    trying out event handlers and states

--- part1/exampleproject/src/App.jsx
+++ part1/exampleproject/src/App.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
+
 const Hello = (props) => {
   return (
     <p>
       Hello {props.name}, age {props.age}
     </p>
+  );
+};
+
+const Newindex = (props) => {
+  const [index, setIndex] = useState(0);
+  return (
+    <button onClick={() => setIndex(index + 1)}>{console.log(index)}</button>
   );
 };
 
@@ -19,6 +28,7 @@ const App = () => {
       <p>
         {a} + {b} = {a + b}
       </p>
+      <Newindex />
     </div>
   );
 };
