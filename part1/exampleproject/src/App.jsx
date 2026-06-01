224ca12cf5e3774ee10230f9974da13228efa8a0
Bas26 <81079172+Bas26@users.noreply.github.com>
2026-06-01 20:52:52 +0700

    playing around with complex states

--- part1/exampleproject/src/App.jsx
+++ part1/exampleproject/src/App.jsx
@@ -1,34 +1,22 @@
 import { useState } from "react";
 
-const Hello = (props) => {
-  return (
-    <p>
-      Hello {props.name}, age {props.age}
-    </p>
-  );
-};
-
-const Newindex = (props) => {
-  const [index, setIndex] = useState(0);
-  return (
-    <button onClick={() => setIndex(index + 1)}>{console.log(index)}</button>
-  );
-};
-
 const App = () => {
-  console.log("Hello Console");
-  const now = new Date();
-  const a = 10;
-  const b = 25;
-  console.log(now, a + b);
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+
   return (
     <div>
-      <Hello name="Alice" age={20} />
-      <Hello name="Bob" age={a + b} />
-      <p>
-        {a} + {b} = {a + b}
-      </p>
-      <Newindex />
+      {left}
+      <button
+        onClick={() => {
+          setLeft(left + 1);
+          console.log("pressed left");
+        }}
+      >
+        left
+      </button>
+      <button onClick={() => setRight(right + 1)}>right</button>
+      {right}
     </div>
   );
 };
