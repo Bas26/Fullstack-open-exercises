3fcf52594b1de7cc98f5394ef4c48445f1138771
Bas26 <81079172+Bas26@users.noreply.github.com>
2026-06-03 19:28:50 +0700

    refactored the code
    learned how to make it more readable by using objects. and learned how to handle arrays

--- part1/exampleproject/src/App.jsx
+++ part1/exampleproject/src/App.jsx
@@ -1,22 +1,31 @@
 import { useState } from "react";
 
 const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
+  const [click, setClick] = useState({
+    left: 0,
+    right: 0,
+  });
+  const [allClicks, setAll] = useState([]);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    setClick({ ...click, left: click.left + 1 });
+    console.log("pressed left");
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    setClick({ ...click, right: click.right + 1 });
+    console.log("pressed right");
+  };
 
   return (
     <div>
-      {left}
-      <button
-        onClick={() => {
-          setLeft(left + 1);
-          console.log("pressed left");
-        }}
-      >
-        left
-      </button>
-      <button onClick={() => setRight(right + 1)}>right</button>
-      {right}
+      {click.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {click.right}
+      {allClicks.join(" ")}
     </div>
   );
 };
