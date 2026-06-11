32fad9f4f756f133f149cff9a984596d9e2dd1f1
Bas26 <81079172+Bas26@users.noreply.github.com>
2026-06-11 18:30:59 +0700

    destructuring and modules

--- part2/exampleproject/src/App.jsx
+++ part2/exampleproject/src/App.jsx
@@ -1,15 +1,13 @@
-const App = (props) => {
-  const { notes } = props;
+import Note from "./Components/Note.jsx";
 
-  const result = notes.map((note) => <li key={note.id}>{note.content}</li>);
+const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {result}
-        <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
       </ul>
     </div>
   );
