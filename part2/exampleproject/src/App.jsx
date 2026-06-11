a9e22a57e211a63405825c9eca758702e39d8e4d
Bas26 <81079172+Bas26@users.noreply.github.com>
2026-06-11 15:25:05 +0700

    understanding map

--- part2/exampleproject/src/App.jsx
+++ part2/exampleproject/src/App.jsx
@@ -1,10 +1,12 @@
 const App = (props) => {
   const { notes } = props;
 
+  const result = notes.map((note) => <li key={note.id}>{note.content}</li>);
   return (
     <div>
       <h1>Notes</h1>
       <ul>
+        {result}
         <li>{notes[0].content}</li>
         <li>{notes[1].content}</li>
         <li>{notes[2].content}</li>
